11cc02b195f4e3e0b22b80ca936f5a9eed1d579c
wangzhiliang03 <wangzhiliang03@baidu.com>
2016-04-24 17:15:36 +0800

    add .babelrc

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,10 +15,7 @@ module.exports = {
             }, {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel', // 'babel-loader' is also a legal name to reference
-                query: {
-                    presets: ['react', 'es2015']
-                }
+                loader: 'babel'
             }, {
                 test: /\.(png|jpg)$/,
                 exclude: /node_modules/,
